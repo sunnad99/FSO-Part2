69d709003e3d96e5dd56eeaa3147a42d3b36355f
Sulaiman Nadeem <s.nadeem@prismware.co>
2023-12-16 23:57:14 +0500

    feat: Completed exercise 2.6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -4,19 +4,39 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = (event) => {
+    console.log("The entered target value is:", event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault();
+
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName(""); // Reset newName input field back to empty string
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handlePersonSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+
+      {/* Rendering notes here */}
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
